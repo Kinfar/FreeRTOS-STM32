fc7c1a6913997f07ced42dfebd898487d680f9af
wnb <188101696@qq.com>
2016-10-08 02:31:40 +0800

    [self][FreeRTOS] init

--- Project/USER/main.c
+++ Project/USER/main.c
@@ -1,30 +1,39 @@
-#include "stm32f10x.h"
- void Delay(u32 count)
- {
-   u32 i=0;
-   for(;i<count;i++);
- }
- int main(void)
- {	
-  GPIO_InitTypeDef  GPIO_InitStructure;
-	 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|
-  RCC_APB2Periph_GPIOE, ENABLE);	  
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;			    
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 	 
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	 
-  GPIO_Init(GPIOB, &GPIO_InitStructure);			  
-  GPIO_SetBits(GPIOB,GPIO_Pin_5);					
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;	           
-  GPIO_Init(GPIOE, &GPIO_InitStructure);	  	    
-  GPIO_SetBits(GPIOE,GPIO_Pin_5); 			  
-  while(1)
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "misc.h"
+
+#include "led.h"
+
+
+void LED0_Task(void * pvParameters);
+void LED1_Task(void * pvParameters);
+
+int main(void)
+{
+	LED_Init();
+	
+	xTaskCreate(LED0_Task, (const char *)"LED0", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
+	xTaskCreate(LED1_Task, (const char *)"LED1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 4, NULL);
+	
+	vTaskStartScheduler();
+}
+
+void LED0_Task(void * pvParameters)
+{
+	while (1)
 	{
-	    GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-	    GPIO_SetBits(GPIOE,GPIO_Pin_5);
-		Delay(3000000);
-		GPIO_SetBits(GPIOB,GPIO_Pin_5);
-		GPIO_ResetBits(GPIOE,GPIO_Pin_5);
-		Delay(3000000);
+		LED0 = !LED0;
+		vTaskDelay(800 / portTICK_RATE_MS);
 	}
- }
+}
+
+void LED1_Task(void * pvParameters)
+{
+	while (1)
+	{
+		LED1 = !LED1;
+		vTaskDelay(500 / portTICK_RATE_MS);
+	}
+}
--- README.md
+++ README.md
@@ -9,6 +9,22 @@ keil5下载地址：http://www.keil.com/demo/eval/arm.htm
 ## 下载固体库包
 可以到openedv的网站进行下载[固体库v3.5.0](http://openedv.com/posts/list/6054.htm)
 
-### 新建项目
+### 新建库函数项目
 clearobj.bat将帮助你清空所有的东西。
 注意填入：STM32F10X_HD,USE_STDPERIPH_DRIVER
+
+##移植FreeRTOS
+改错：
+> ..\OBJ\FreeRTOS.axf: Error: L6218E: Undefined symbol vApplicationTickHook (referred from tasks.o).
+
+在sys.c中加入函数
+
+```
+void vApplicationTickHook(void)
+{
+  
+}
+```
+
+## 相关参考网站
+http://www.openedv.com/thread-60571-1-1.html
